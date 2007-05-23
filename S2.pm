6dce8268ee7c7741f3bc773312b965bffadd0dbb
Martin Atkins <mart@degeneration.co.uk>
2007-05-23 01:27:10 +0000

    Missed part of the old ghetto nullness logic.

--- S2.pm
+++ S2.pm
@@ -534,7 +534,7 @@ sub get_characters
 
 sub check_defined {
     my $obj = shift;
-    return UNIVERSAL::isa($obj, 'S2::Object') || (ref $obj eq 'HASH' && defined($obj->{_type}));
+    return UNIVERSAL::isa($obj, 'S2::Object') || (ref $obj eq 'HASH' && defined($obj->{_type}) && ! $obj->{_isnull});
 }
 
 sub check_elements {
